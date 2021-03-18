38d7960b8ff4bbcbe67d13763571e4b5aca19e63
ffrastio <48426886+ffrastio@users.noreply.github.com>
2021-03-18 11:39:53 +0700

    Bootstrap

---
+++ frontend/src/components/layouts/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" class="container">
+            <b-navbar-brand href="#">
+                <img
+                    src="@/assets/img/logo.png"
+                    alt="Logo UP2M"
+                    height="50"
+                    class="ima"
+                />
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="hamburger"></b-navbar-toggle>
+
+            <b-collapse id="hamburger" is-nav>
+                <b-navbar-nav class="mx-auto text-left">
+                    <b-nav-item href="#" class="nav-item">Home</b-nav-item>
+                    <b-nav-item href="#" class="nav-item">About</b-nav-item>
+                    <b-nav-item href="#" class="nav-item">Author</b-nav-item>
+                    <b-nav-item-dropdown text="Sources" left>
+                        <b-dropdown-item href="#">Penelitian</b-dropdown-item>
+                        <b-dropdown-item href="#">Pengabdian</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav-item {
+    font-weight: bold;
+
+    :hover {
+        background-color: #008797;
+        color: #fff !important;
+    }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <Navbar />
+        <div>
+            <b-jumbotron
+                header="BootstrapVue"
+                lead="Bootstrap v4 Components for Vue.js 2"
+            >
+                <p>For more information visit website</p>
+                <b-button variant="primary" href="#">More Info</b-button>
+            </b-jumbotron>
+        </div>
+        <div class="text-left px-4">
+          <h1 >Data Statistik</h1>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from "@/components/layouts/Navbar.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: {
+        Navbar
+    }
+};
 </script>
