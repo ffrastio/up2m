707997b1656642bafeeac5b8b75522ba933adc26
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2021-03-27 16:47:15 +0700

    db migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->increments('id');
+            $table->char('nidn')->unique();
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'pimpinan']);
             $table->rememberToken();
             $table->timestamps();
         });
