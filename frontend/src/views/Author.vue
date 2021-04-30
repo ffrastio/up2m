7f20d51b89bc60b684935fed434be21bb76ab6f7
ffrastio <48426886+ffrastio@users.noreply.github.com>
2021-05-01 05:45:34 +0700

    update author Vue

--- frontend/src/views/Author.vue
+++ frontend/src/views/Author.vue
@@ -56,13 +56,6 @@
                 class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-full text-sm focus:outline-none"
                 @keyup="searchAuthor"
             />
-            <div class="p-4">
-                <button
-                    class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
-                >
-                    icon
-                </button>
-            </div>
         </div>
         <div class=" flex items-center ">
             <div class="overflow-x-auto w-full">
